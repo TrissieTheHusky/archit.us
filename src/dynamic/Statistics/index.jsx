8360fa22a79c3bc4bd31ed6229656fb49d4e7967
Lucas Phillips <lucas.couto.phillips@gmail.com>
2019-12-29 15:53:09 -0500

    pull member/message count from state

--- src/dynamic/Statistics/index.jsx
+++ src/dynamic/Statistics/index.jsx
@@ -3,22 +3,29 @@ import Histogram from "components/Histogram";
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { getGuildStats } from "store/actions";
-import { useDispatch } from "react-redux";
 import "./style.scss";
 
 function Statistics({ guildId }) {
   const dispatch = useDispatch();
 
+  const { memberCount, messageCount } = useSelector(state => {
+    return {
+      memberCount: state.stats.memberCount,
+      messageCount: state.stats.messageCount
+    };
+  });
+
   useEffect(() => {
     dispatch(getGuildStats(guildId));
-  }, []);
+  }, [guildId]);
 
   return (
     <Container className="statistics-container" fluid>
       <h2>Statistics</h2>
-      <Counter value={1234} title={"Members"} />
-      <Counter value={502643} title={"Messages"} />
+      <Counter value={memberCount} title={"Members"} />
+      <Counter value={messageCount} title={"Messages"} />
       <Histogram />
     </Container>
   );
